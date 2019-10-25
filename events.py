e25ad57dba8ff8c7670dbfa9a3e400c7fbe2c9fb
Benjamin Knutson <b3nknutson@gmail.com>
2019-10-25 03:27:53 -0400

    added a lot, began merging the two scripts.

--- events.py
+++ events.py
@@ -4,8 +4,10 @@ import discord
 import random
 import schedule
 import time
+import asyncio
 
-   
+from asgiref.sync import async_to_sync, sync_to_async
+
 
 client = discord.Client()
 class town:
@@ -87,84 +89,98 @@ def load_discord_token():
 def random_event(town):
     pass
 
-async def upkeep_event():
-    for town in towns:
-        res=town.get_res()
-        return_string=""
-        roll=random.randint(1,101)
-        if roll > 0 and roll < 11:
-            res[6]=res[6]-res[11]
-            return_string ="No income, bad day. Upkeep cost still applies\n Income ="+str(-res[11])
-        elif roll > 10 and roll < 51:
-            res[6]=res[6]-res[11]+res[10]
-            return_string="standard day, profit =income-upkeep\n income ="+str(-res[11]+(res[10]))
-        elif roll > 50 and roll < 61:
-            res[6]=res[6]-res[11]+1.25*(res[10])
-            return_string ="Good day, profit =(income-upkeep)*1.25 25% more profit\n Income ="+str(-res[11]+1.25*(res[10]))
-        elif roll > 60 and roll < 81:
-            res[6]=res[6]-res[11]+1.5*(res[10])
-            return_string= "Good day, profit =(income-upkeep)*1.5 50% more profit\n Income ="+str(-res[11]+1.5*(res[10]))
-        elif roll > 80 and roll < 91:
-            res[6]=res[6]-res[11]+1.75*(res[10])
-            return_string="Good day, profit =(income-upkeep)*1.75 75% more profit\n Income ="+str(-res[11]+1.75*(res[10]))
-        elif roll > 90 and roll < 101:
-            res[6]=res[6]-res[11]+2*(res[10])
-            return_string ="Good day, profit =(income-upkeep)*2 100% more profit\nIncome ="+str(-res[11]+2*(res[10]))
+def upkeep_event(town_pass):
+    res=town_pass.get_res()
+    return_string=""
+    roll=random.randint(1,101)
+    if roll > 0 and roll < 11:
+        res[5]=int(res[5]-res[11])
+        return_string ="No income, bad day. Upkeep cost still applies\n Income ="+str(-res[11])
+    elif roll > 10 and roll < 51:
+        res[5]=int(res[5]-res[11]+res[10])
+        return_string="standard day, profit =income-upkeep\n income ="+str(-res[11]+(res[10]))
+    elif roll > 50 and roll < 61:
+        res[5]=int(res[5]-res[11]+1.25*(res[10]))
+        return_string ="Good day, profit =(income-upkeep)*1.25 25% more profit\n Income ="+str(-res[11]+1.25*(res[10]))
+    elif roll > 60 and roll < 81:
+        res[5]=int(res[5]-res[11]+1.5*(res[10]))
+        return_string= "Good day, profit =(income-upkeep)*1.5 50% more profit\n Income ="+str(-res[11]+1.5*(res[10]))
+    elif roll > 80 and roll < 91:
+        res[5]=int(res[6]-res[11]+1.75*(res[10]))
+        return_string="Good day, profit =(income-upkeep)*1.75 75% more profit\n Income ="+str(-res[11]+1.75*(res[10]))
+    elif roll > 90 and roll < 101:
+        res[5]=int(res[5]-res[11]+2*(res[10]))
+        return_string ="Good day, profit =(income-upkeep)*2 100% more profit\nIncome ="+str(-res[11]+2*(res[10]))
         
-        res[0]+=int(res[0]*.1)#assumine 10% growth per week, will adjust as needed    
-        res[1]+=int(res[6])
-        res[2]+=int(res[7])
-        res[3]+=int(res[8])
-        res[4]+=int(res[9])
-        town.set_res(res)
-        town.save_to_file
-        return_string+="\n"+town.resource_totals()
-        town.set_message(return_string)
-        town.save_to_file()
-        await message_perhaps()
-    
-    
-
-    
-
-    
-
-
-
-schedule.every(1).minutes.do(upkeep_event)
-schedule.every(2).minutes.do(random_event)
-
-    
-
-async def message_perhaps():
-    client.login(discord_token)
-    for town in towns:
-        discord_channel=client.get_channel(town.get_channel())
-        await discord_channel.send(town.get_message())
+    res[0]+=int(res[0]*.1)#assumine 10% growth per week, will adjust as needed    
+    res[1]+=int(res[6])
+    res[2]+=int(res[7])
+    res[3]+=int(res[8])
+    res[4]+=int(res[9])
+    town_pass.set_res(res)
+    town_pass.save_to_file
+    return_string+="\n"+town_pass.resource_totals()
+    town_pass.set_message(return_string)
+    town_pass.save_to_file()
+    #print (town_pass.get_message())
+    return return_string
         
     
     
+
+  
 
 
 @client.event
 async def on_ready():
-    await upkeep_event
+    
     print(f'{client.user} has connected to Discord!')
-    for town in towns:
-        discord_channel=client.get_channel(town.get_channel())
-        await discord_channel.send(town.get_message())
     
-    while True:
-        schedule.run_pending()
-        print ("tick")
-        time.sleep(5)
+@client.event
+async def on_message(message):
+    if message.author==client.user:
+        return
+    
+    msg=str.split(str.lower(message.content))
+    if len(msg)==3:
+        if msg[1]=="totals" and msg[2]=="please":
+            for each in towns:
+                if str.lower(each.get_name())==msg[0]:
+                    await message.channel.send(each.resource_totals())
+   
+    if message.content=="help":
+        await message.channel.send("Commands: 'List towns', 'townname totals please', 'help''")
+        pass
+    if message.content=="list towns":
+        return_string=""
+        for each in towns:
+            return_string+=each.get_name()+"\n"
+            await message.channel.send(return_string)
+        pass
+    
+        
+    #if message.content =='base productiom please':
+     #   await message.channel.send(production_totals())
+      #  print(production_totals())
+    
+    
     
    
+async def events_rotation():
+    await client.wait_until_ready()
+    print("event rotation started")
+    while client.is_closed:
         
+        for each in towns:
+            
+            discord_channel=client.get_channel(each.get_channel())
+            return_string=await sync_to_async(upkeep_event)(each)
+            print(return_string+"")
+            await discord_channel.send(return_string)
+        await asyncio.sleep(60)
+    print("?")
+    pass
         
-    
-   # await discord_channel.send('I have awakened')
-   
     
 #load towns
 towns=[]
@@ -185,6 +201,7 @@ token = os.getenv('DISCORD_TOKEN')
 
 discord_token=load_discord_token()
 
+client.loop.create_task(events_rotation())
 client.run(load_discord_token())
 
 
